8c235e97757b1b1ddd476a818880abf4816268a5
Mariusz Gamrat <m.gamrat@otcf.pl>
2023-01-08 19:35:09 +0100

    added routing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hi there";
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('events', 'DefaultController');
+
+Routing::run($path);
