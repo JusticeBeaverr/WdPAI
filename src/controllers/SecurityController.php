3554ca60392ac802d4cb89d9389709c4dc7a8614
Mariusz Gamrat <m.gamrat@otcf.pl>
2023-02-06 16:04:53 +0100

    add-event

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,26 +2,36 @@
 
 require_once 'AppController.php';
 require_once __DIR__ .'/../models/User.php';
+require_once __DIR__ . '/../repository/UserRepository.php';
 class SecurityController extends AppController{
+    private UserRepository $userRepository;
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->userRepository = new UserRepository();
+    }
     public function login()
     {
-        $user = new User('jsnow@pk.edu.pl', "nowak21",'admin');
-
         if (!$this->isPost()) {
             return $this->render('login');
         }
 
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-        if ($user->getUsername() !== $username) {
-            return $this->render('login', ['messages' => ['User with this username not exist!']]);
+        try {
+            $user = $this->userRepository->getUser($username);
+        } catch (Exception $error) {
+            return $this->render('login', ["messages" => [$error->getMessage()]]);
         }
 
-        if ($user->getPassword() !== $password) {
+        if (!password_verify($password, $user->getPassword())) {
             return $this->render('login', ['messages' => ['Wrong password!']]);
         }
+
+        $_SESSION['user_id'] = $this->userRepository->getId();
+        $_SESSION['user_details'] = $user->getName() . ' ' . $user->getSurname();
 
         $url = "http://$_SERVER[HTTP_HOST]";
         header("Location: {$url}/events");
